334cce123c5b637d917eeebf597459fabc0c2b29
vagrant <vagrant@homestead>
2018-05-04 06:24:54 +0000

    First commit PHP

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="ja" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title>Calendar</title>
+   </head>
+   <body>
+     <table>
+       <thead>
+         <tr>
+           <th><a href="#">&laquo;</a> </th>
+           <th colspan="5"> August 2015</th>
+           <th><a href="#">&laquo;</a> </th>
+         </tr>
+       </thead>
+         <tbody>
+         </tbody>
+         <tfoot>
+           <th colspan="7"><a href="#">Today</a> </th>
+         </tfoot>
+     </table>
+   </body>
+ </html>
